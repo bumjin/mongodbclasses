69b17c2c8285bd6a0fe46c22fa39788990b26da3
bumjin <bumjin@gmail.com>
2012-10-27 18:14:16 +0900

    python for loop combine dict and list

--- classes/M101/for_loop_dict.py
+++ classes/M101/for_loop_dict.py
@@ -1,6 +1,15 @@
 
 
-person = {'name':'bumjin','favorite_color':'blue','hair':'black'}
+person = {'name':'bumjin','favorite_color':'blue',
+		'hair':'black','interests' : 
+			['cycling', 'running', 'biking']}
 
 for key in person:	#iterate
-	print "key is "+key+" value is "+person[key]
+
+	if (key == 'interests'):
+		print "interests...."
+		for interest in person[key]:
+			print "\tinterest is "+interest
+
+	else:
+		print "key is "+key+" value is "+person[key]
