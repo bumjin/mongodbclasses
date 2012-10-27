06cbef587594b1d0ae95ba70bd2a1e22e2bfc939
bumjin <bumjin@gmail.com>
2012-10-27 22:31:33 +0900

    using cookie

--- classes/M101/fruit_form.py
+++ classes/M101/fruit_form.py
@@ -14,6 +14,12 @@ def favorite_fruit():
 	if( fruit == None or fruit == ""):
 		fruit = "No Fruit Selected"
 
+	bottle.response.set_cookie('fruit', fruit)
+	bottle.redirect('/show_fruit')
+
+@bottle.route('/show_fruit')
+def show_fruit():
+	fruit = bottle.request.get_cookie('fruit')
 	#return bottle.template('fruit_selection', fruit=fruit)
 	return bottle.template('fruit_selection.tpl', {'fruit': fruit})
 
